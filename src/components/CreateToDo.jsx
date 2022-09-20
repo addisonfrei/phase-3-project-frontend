28508e54d0a84c47bb3eff109dc4e74849b6b490
Addison <addison.frei@icloud.com>
2022-09-20 16:28:25 -0400

    creating generic form in CreateToDo

--- src/components/CreateToDo.jsx
+++ src/components/CreateToDo.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const CreateToDo = () => {
   return (
-    <div>CreateToDo</div>
+    <div>
+      <h2>Create a To Do</h2>
+      <form>
+        <div>
+          <label>Task:</label>
+          <input type='text'/>
+          <label>Category:</label>
+          <select id='category' name='category'>
+            <option value='home'>Home</option>
+            <option value='pet'>Pet</option>
+            <option value='vehicle'>Vehicle</option>
+            <option value='miscellaneous'>Miscellaneous</option>
+          </select>
+        </div>
+          <br></br>
+          <input type="submit" />
+      </form>
+    </div>
   )
 }
 
