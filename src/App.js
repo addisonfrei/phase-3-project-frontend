ead221580f5ec0b740842ab7b4a13ef6a099a6e4
Addison <addison.frei@icloud.com>
2022-09-20 14:20:06 -0400

    cleaing up App component and test DB connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+  const [test, setTest] = useState('')
+
+  useEffect( () => {
+    fetch('http://localhost:9292/')
+      .then(r => r.json())
+      .then(setTest)
+  }, [])
+  console.log(test)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {test.message}
     </div>
   );
 }
