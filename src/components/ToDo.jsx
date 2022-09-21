85cb0d57dc4634a0693671e550b211480248ae96
Addison <addison.frei@icloud.com>
2022-09-21 13:46:18 -0400

    handleCheck added

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -4,14 +4,18 @@ const ToDo = ( { toDo } ) => {
   
   // function will send Delete request to DB
   function handleDelete(e) {
-    console.log(e)
+    console.log(toDo)
+  }
+  function handleCheck(e) {
+    console.log(toDo)
+    console.log("Checked")
   }
   return (
     <li>
       <span>{toDo.task}</span>
       <span>{toDo.category}</span>
       <span>{toDo.dueDate}</span>
-      <input style={{marginRight: "100px"}} type='checkbox' />
+      <input style={{marginRight: "100px"}} type='checkbox' onClick={handleCheck}/>
       <button onClick={handleDelete}>Delete</button>
     </li>
   )
