5dfde257067d79596ad5c920d05f79c5020ccc0d
Addison <addison.frei@icloud.com>
2022-09-21 12:20:18 -0400

    adding DatePicker for calendar use in form

--- src/components/CreateToDo.jsx
+++ src/components/CreateToDo.jsx
@@ -1,27 +1,38 @@
 import React, { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
 
 const CreateToDo = () => {
-  // Make a controlled form with inputs adjusted below.  
-  // State?  New state here or on App passed as prop?
+  
+  const [ date, setDate ] = useState(new Date());
   const [ formData, setFormData ] = useState({
     "task": "",
-    "category": "home"
-  })
-
+    "category": "home",
+    "dueDate": date.toDateString(),
+    "completed": false
+  });
+  
+  // Handles task and category changes
   function handleFormChange(e) {
     setFormData({...formData,
       [e.target.name]: e.target.value
     })
   }
-  console.log("formData:", formData)
+  // Handles calendar change due to event being the date.  No target included
+  function handleCalendarChange(e) {
+    setFormData({...formData,
+      "dueDate": e.toDateString()
+    })
+  }
+  console.log(formData)
 
   return (
     <div>
-      <h2>Create a To Do</h2>
+      <h2>Create a Task</h2>
       <form>
         <div>
-          <label>Task:</label>
-          <input type='text' id='task' name='task' value={formData.task} onChange={handleFormChange}/>
+          <label htmlFor='task'>Task:</label>
+          <input type='text' id='task' name='task' value={formData.task} autoFocus={true} onChange={handleFormChange}/>
           <label>&nbsp;Category:</label>
           <select id='category' name='category' onChange={handleFormChange}>
             <option value='home'>Home</option>
@@ -29,9 +40,12 @@ const CreateToDo = () => {
             <option value='vehicle'>Vehicle</option>
             <option value='miscellaneous'>Miscellaneous</option>
           </select>
+          <br></br>
+          <label>Due By:</label>
+          <DatePicker selected={date} onSelect={setDate} onChange={handleCalendarChange}/>
         </div>
           <br></br>
-          <input type="submit" value="Create Task" />
+          <input type="submit" value="Add Task" />
       </form>
     </div>
   )
