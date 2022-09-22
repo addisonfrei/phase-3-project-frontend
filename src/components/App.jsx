515264a067d110be10591a7030ae7b4973697fc2
Addison <addison.frei@icloud.com>
2022-09-22 13:01:35 -0400

    added state and fetch for categories

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,9 +9,43 @@ import TaskContainer from './TaskContainer';
 
 
 const App = () => {
-    const [taskList, setTaskList] = useState([])
+    const [ taskList, setTaskList ] = useState([])
+    const [ categories, setCategories ] = useState([])
+    
+    //Fetch Task data from database
+    useEffect(() => {
+        fetch('http://localhost:9292/tasks')
+         .then(r => r.json())
+         .then(setTaskList)
+    }, [])
 
-    // Fake data before fetching from database
+    //Fetch Categories from database
+    useEffect(() => {
+        fetch('http://localhost:9292/categories')
+         .then(r => r.json())
+         .then(setCategories)
+    }, [])
+
+    console.log("TaskList",taskList)
+    console.log("Categories:", categories)
+
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path='/' element={ <Home /> }/>
+                <Route path='/tasks' element={ <TaskContainer taskList={taskList} /> }/>
+                <Route path='/addtask' element={ <CreateTask /> }/>
+                <Route path='/addcategory' element={<CreateCategory />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App
+
+
+  // Fake data before fetching from database
     // {id: 1,
     //     task: "Walk Dog", 
     //     category: "pets",
@@ -22,29 +56,3 @@ const App = () => {
     //     category: "home",
     //     dueDate: "Wed Sep 21 2022", 
     //     completed: false}
-    
-
-    
-    //Fetch data from database
-    useEffect(() => {
-        fetch('http://localhost:9292/viewtasks')
-         .then(r => r.json())
-         .then(setTaskList)
-    }, [])
-
-    console.log(taskList)
-
-    return (
-        <Router>
-            <NavBar />
-            <Routes>
-                <Route path='/' element={ <Home /> }/>
-                <Route path='/viewtasks' element={ <TaskContainer taskList={taskList} /> }/>
-                <Route path='/addtask' element={ <CreateTask /> }/>
-                <Route path='/addcategory' element={<CreateCategory />} />
-            </Routes>
-        </Router>
-    )
-}
-
-export default App
